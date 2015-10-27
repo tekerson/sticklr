b963f4cd5a753165ed6af7f999a4396c1a9db41c
Brenton Alker <brenton@tekerson.com>
2015-10-27 19:18:43 +0100

    Add sticker upload

--- app/components/sticker-upload/index.js
+++ app/components/sticker-upload/index.js
@@ -1,10 +1,39 @@
+import mkSticker from "sticklr/sticker";
 
-export default () => ({
+export default (stickerBook) => ({
   restrict: "E",
+  controllerAs: "vm",
+
   scope: {},
+
+  controller: ($scope) => {
+    let upload = (sticker) => {
+      if (sticker.dataUri == null) {
+        return;
+      }
+      if (sticker.name == null) {
+        return;
+      }
+      stickerBook.addSticker(mkSticker(sticker.name, sticker.dataUri));
+    };
+
+    $scope.sticker = {
+      name: null,
+      dataUri: null
+    };
+
+    return {
+      upload
+    };
+  },
+
   template: `
     <div class="sticker-upload">
-      <button>Upload Sticker</button>
+      <form ng-submit="vm.upload(sticker)">
+        <input type="text" name="name" ng-model="sticker.name" />
+        <stk-photo-field on-upload="sticker.dataUri = dataUri"></stk-photo-field>
+        <button>Upload Sticker</button>
+      </form>
     </div>
   `
 });
