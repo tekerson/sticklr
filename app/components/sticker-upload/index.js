8e1bd0db914dd72e8d355e3695c067f33303f2b2
Brenton Alker <brenton@tekerson.com>
2015-10-27 19:44:12 +0100

    Show/Hide sticker upload form and disable submit

--- app/components/sticker-upload/index.js
+++ app/components/sticker-upload/index.js
@@ -8,31 +8,52 @@ export default (stickerBook) => ({
 
   controller: ($scope) => {
     let upload = (sticker) => {
-      if (sticker.dataUri == null) {
-        return;
-      }
-      if (sticker.name == null) {
+      if (!isValid(sticker)) {
         return;
       }
       stickerBook.addSticker(mkSticker(sticker.name, sticker.dataUri));
+      hideUpload();
     };
 
+    let showUpload = () => {
+      $scope.showingUpload = true;
+    };
+    let hideUpload = () => {
+      $scope.showingUpload = false;
+    };
+
+    let isValid = (sticker) => {
+      if (sticker.dataUri == null) {
+        return false;
+      }
+      if (sticker.name == null) {
+        return false;
+      }
+      return true;
+    };
+
+    $scope.showingUpload = false;
     $scope.sticker = {
       name: null,
       dataUri: null
     };
 
     return {
-      upload
+      upload,
+      isValid,
+      showUpload,
+      hideUpload
     };
   },
 
   template: `
     <div class="sticker-upload">
-      <form ng-submit="vm.upload(sticker)">
+      <button ng-click="vm.showUpload()" ng-if="!showingUpload">Upload Sticker</button>
+      <form ng-submit="vm.upload(sticker)" ng-if="showingUpload">
         <input type="text" name="name" ng-model="sticker.name" />
         <stk-photo-field on-upload="sticker.dataUri = dataUri"></stk-photo-field>
-        <button>Upload Sticker</button>
+        <button ng-disabled="!vm.isValid(sticker)">Submit</button>
+        <button ng-click="vm.hideUpload()">Cancel</button>
       </form>
     </div>
   `
